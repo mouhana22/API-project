3445734d53dd82fa690b261afe6e1a4a546be070
mouhana22 <mouhana1422@gmail.com>
2024-02-09 22:11:30 +0300

    Database Migration and Model

---
+++ app/Models/Property.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Property extends Model
+{
+    use HasFactory;
+
+    protected $table = 'properties';
+
+    protected $fillable = [
+        'title',
+        'address', 
+        'price',
+        'bedrooms',
+        'bathrooms',
+        'type',
+        'status'
+    ];
+}
